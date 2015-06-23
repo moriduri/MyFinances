65141221a9b4637df0100ec115ceece19152bc8d
Dmitry <spandei@yandex.ru>
2015-06-23 16:46:58 +0300

    Added database

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,11 +6,24 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);    
+    ui->setupUi(this);
+
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("Finances.sqlite");
+    if (!db.open())
+        qFatal("Failed to connect to database.");
+
+    QSqlQuery query(db);
+    if (!query.exec("CREATE TABLE IF NOT EXISTS transactions (id INTEGER UNIQUE PRIMARY KEY)"))
+        qFatal("Cannot create or open a table in the database");
+
+    if (!query.exec("CREATE TABLE IF NOT EXISTS categories (id INTEGER UNIQUE PRIMARY KEY)"))
+        qFatal("Cannot create or open a table in the database");
 }
 
 MainWindow::~MainWindow()
 {
+    db.close();
     delete ui;
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtSql>
 
 namespace Ui {
 class MainWindow;
@@ -20,6 +21,8 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
+
+    QSqlDatabase db;
 };
 
 #endif // MAINWINDOW_H
