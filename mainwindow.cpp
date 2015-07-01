ac0548672720ea9625976774e8774758b113e370
Dmitry <spandei@yandex.ru>
2015-07-01 17:52:34 +0300

    added columns to database

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,11 +14,21 @@ MainWindow::MainWindow(QWidget *parent) :
         qFatal("Failed to connect to database.");
 
     QSqlQuery query(db);
-    if (!query.exec("CREATE TABLE IF NOT EXISTS transactions (id INTEGER UNIQUE PRIMARY KEY)"))
+    if (!query.exec("CREATE TABLE IF NOT EXISTS categories ("
+                    "id INTEGER PRIMARY KEY,"
+                    "description TEXT"
+                    ")"))
         qFatal("Cannot create or open a table in the database");
 
-    if (!query.exec("CREATE TABLE IF NOT EXISTS categories (id INTEGER UNIQUE PRIMARY KEY)"))
-        qFatal("Cannot create or open a table in the database");
+    if (!query.exec("CREATE TABLE IF NOT EXISTS transactions ("
+                    "id INTEGER PRIMARY KEY,"
+                    "category_id INTEGER NOT NULL,"
+                    "description TEXT,"
+                    "sum INTEGER,"
+                    "transaction_date DATE,"
+                    "FOREIGN KEY(category_id) REFERENCES categories(id)"
+                    ")"))
+        qFatal("Cannot create or open a table in the database: ");
 }
 
 MainWindow::~MainWindow()
